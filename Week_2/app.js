5b0e4c8c1105e1c458919c5bea364ebda9d59369
TanPhuocHuynh <78418185+tanhuynh230399@users.noreply.github.com>
2024-01-18 16:27:39 +1100

    Add files via upload

--- Week_2/app.js
+++ Week_2/app.js
@@ -1,81 +1,132 @@
+
+
+let jsonData;
+// while (!jsonData)
+//   {
+//   fetch('https://randomuser.me/api/', { method: 'GET' })
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     // Process the data as needed
+    
+//     jsonData = data;
+//     // console.log(jsonData);
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   }); 
+//   }
+
 document.addEventListener('DOMContentLoaded', function() {
-    // JSON data
-    const jsonData = {
+
+  
+  console.log(jsonData);
+
+  if(jsonData)
+  {
+  
+  // // JSON data
+    // jsonData = {
         
-        "results": [
-            {
-              "gender": "Female",
-              "name": {
-                "title": "Miss",
-                "first": "Nurdan",
-                "last": "Günday"
-              },
-              "location": {
-                "street": {
-                  "number": 5913,
-                  "name": "Kushimoto Sk"
-                },
-                "city": "Manisa",
-                "state": "Yozgat",
-                "country": "Turkey",
-                "postcode": 78008,
-                "coordinates": {
-                  "latitude": "71.9630",
-                  "longitude": "154.9061"
-                },
-                "timezone": {
-                  "offset": "+3:30",
-                  "description": "Tehran"
-                }
-              },
-              "email": "nurdan.gunday@example.com",
-              "login": {
-                "uuid": "b609938d-c799-424b-b4b6-983e68e20a1f",
-                "username": "happymeercat586",
-                "password": "post",
-                "salt": "CMKlqRor",
-                "md5": "7efd52e14885587e82df8a7ec10a4e9f",
-                "sha1": "2cec93899148776e6512d4b3f161863dc7fe52f0",
-                "sha256": "93f9b6e2c55d4ebb4bc3297bece3503653c2203ab875ab5cf619ecab95d3097a"
-              },
-              "dob": {
-                "date": "1957-08-04T07:06:45.979Z",
-                "age": 66
-              },
-              "registered": {
-                "date": "2020-12-03T05:18:42.608Z",
-                "age": 3
-              },
-              "phone": "(591)-060-9077",
-              "cell": "(324)-040-1371",
-              "id": {
-                "name": "",
-                "value": null
-              },
-              "picture": {
-                "large": "https://randomuser.me/api/portraits/women/36.jpg",
-                "medium": "https://randomuser.me/api/portraits/med/women/36.jpg",
-                "thumbnail": "https://randomuser.me/api/portraits/thumb/women/36.jpg"
-              },
-              "nat": "TR"
-            }
-          ],
-          "info": {
-            "seed": "624223d933c2433b",
-            "results": 1,
-            "page": 1,
-            "version": "1.4"
-          }
+    //     "results": [
+    //         {
+    //           "gender": "Female",
+    //           "name": {
+    //             "title": "Miss",
+    //             "first": "Nurdan",
+    //             "last": "Günday"
+    //           },
+    //           "location": {
+    //             "street": {
+    //               "number": 5913,
+    //               "name": "Kushimoto Sk"
+    //             },
+    //             "city": "Manisa",
+    //             "state": "Yozgat",
+    //             "country": "Turkey",
+    //             "postcode": 78008,
+    //             "coordinates": {
+    //               "latitude": "71.9630",
+    //               "longitude": "154.9061"
+    //             },
+    //             "timezone": {
+    //               "offset": "+3:30",
+    //               "description": "Tehran"
+    //             }
+    //           },
+    //           "email": "nurdan.gunday@example.com",
+    //           "login": {
+    //             "uuid": "b609938d-c799-424b-b4b6-983e68e20a1f",
+    //             "username": "happymeercat586",
+    //             "password": "post",
+    //             "salt": "CMKlqRor",
+    //             "md5": "7efd52e14885587e82df8a7ec10a4e9f",
+    //             "sha1": "2cec93899148776e6512d4b3f161863dc7fe52f0",
+    //             "sha256": "93f9b6e2c55d4ebb4bc3297bece3503653c2203ab875ab5cf619ecab95d3097a"
+    //           },
+    //           "dob": {
+    //             "date": "1957-08-04T07:06:45.979Z",
+    //             "age": 66
+    //           },
+    //           "registered": {
+    //             "date": "2020-12-03T05:18:42.608Z",
+    //             "age": 3
+    //           },
+    //           "phone": "(591)-060-9077",
+    //           "cell": "(324)-040-1371",
+    //           "id": {
+    //             "name": "",
+    //             "value": null
+    //           },
+    //           "picture": {
+    //             "large": "https://randomuser.me/api/portraits/women/36.jpg",
+    //             "medium": "https://randomuser.me/api/portraits/med/women/36.jpg",
+    //             "thumbnail": "https://randomuser.me/api/portraits/thumb/women/36.jpg"
+    //           },
+    //           "nat": "TR"
+    //         }
+    //       ],
+    //       "info": {
+    //         "seed": "624223d933c2433b",
+    //         "results": 1,
+    //         "page": 1,
+    //         "version": "1.4"
+    //       }
           
-    };
+    // };
+    // console.log(jsonData);
+
+
   
     // Function to create user profile HTML
     function createUserProfile(user) {
       const userProfile = document.querySelector('#userProfile');
-      userProfile.innerHTML = `
-        <img src="${user.picture.large}" style= "justify-content: center; align-items: center; display: flex" alt="User Profile Picture" class="profile-picture">
-        <h1>${user.name.title} ${user.name.first} ${user.name.last}</h1>
-      `;
+      // userProfile.innerHTML = `
+      //   <img src="${user.picture.large}" style= "justify-content: center; align-items: center; display: flex" alt="User Profile Picture" class="profile-picture">
+      //   <h1>${user.name.title} ${user.name.first} ${user.name.last}</h1>
+      // `;
+      // userProfile.classList.add('');
+      const img_profile = document.createElement('img');
+      img_profile.src = user.picture.large;
+      img_profile.style.borderRadius = "50%";
+      img_profile.style.border = "3px solid #333";
+      const pre_name_text = document.createElement('p');
+      pre_name_text.textContent = "Hi! My name is";
+      pre_name_text.style.marginBottom = "0px";
+      pre_name_text.style.color = "gray";
+      const name_text = document.createElement('h1');
+      name_text.textContent = user.name.first + " " + user.name.last;
+      name_text.style.marginTop = '5px';
+      name_text.style.fontWeight = "lighter";
+
+
+      userProfile.appendChild(img_profile);
+      userProfile.appendChild(pre_name_text);
+      userProfile.appendChild(name_text);
     }
     function createDisplayBox()
     {
@@ -111,6 +162,7 @@ document.addEventListener('DOMContentLoaded', function() {
             temp_icon_list_item.style.borderRadius = '12px';
             temp_icon_list_item.style.textAlign = 'center';
             
+            
             if (i == 0)
             {
                 temp_icon_list_item.textContent = 'Gender';
@@ -140,28 +192,37 @@ document.addEventListener('DOMContentLoaded', function() {
 
     }
 
-
   
     // Call the function with the first user from the JSON data
     createUserProfile(jsonData.results[0]);
     createIconBoxes();
     createDisplayBox();
-
+    const test = document.querySelector("#userProfile");
+    const test_2 = document.createElement('div');
+    test_2.id = "arrowBounce";
+    test.appendChild(test_2);
     const display_box_text = document.querySelector('#displayBox');
     const original_text = display_box_text.textContent;
     const icon_box_list = document.querySelectorAll('#listOfInfo p');
-    console.log(icon_box_list);
+    // console.log(icon_box_list);
     const gender_ = document.querySelector('#section0');
     const gender_updatedtext = jsonData.results[0].gender;
+
+    let flex_index = 0;
+
     gender_.addEventListener("mouseover", function(){
         display_box_text.textContent = gender_updatedtext;
         display_box_text.classList.add('smooth-transition');
         icon_box_list[0].style.backgroundColor = '#e8e0d3';
+        icon_box_list[0].classList.add('bounce');
+        // test_2.classList.add('arrow');
+        // test_2.classList.add('bounce');
     });
     gender_.addEventListener("mouseout", function(){
         display_box_text.textContent = original_text;
         display_box_text.classList.remove('smooth-transition');
         icon_box_list[0].style.backgroundColor = '#f8f0e3';
+        icon_box_list[0].classList.remove('bounce');
     });
 
     const email_ = document.querySelector('#section1');
@@ -170,11 +231,13 @@ document.addEventListener('DOMContentLoaded', function() {
         display_box_text.textContent = email_updatedtext;
         display_box_text.classList.add('smooth-transition');
         icon_box_list[1].style.backgroundColor = '#e8e0d3';
+        icon_box_list[1].classList.add('bounce');
     });
     email_.addEventListener("mouseout", function(){
         display_box_text.textContent = original_text;
         display_box_text.classList.remove('smooth-transition');
         icon_box_list[1].style.backgroundColor = '#f8f0e3';
+        icon_box_list[1].classList.remove('bounce');
     });
 
     const location_ = document.querySelector('#section2');
@@ -183,11 +246,13 @@ document.addEventListener('DOMContentLoaded', function() {
         display_box_text.textContent = location_updatedtext;
         display_box_text.classList.add('smooth-transition');
         icon_box_list[2].style.backgroundColor = '#e8e0d3';
+        icon_box_list[2].classList.add('bounce');
     });
     location_.addEventListener("mouseout", function(){
         display_box_text.textContent = original_text;
         display_box_text.classList.remove('smooth-transition');
         icon_box_list[2].style.backgroundColor = '#f8f0e3';
+        icon_box_list[2].classList.remove('bounce');
     });
 
     const phone_ = document.querySelector('#section3');
@@ -196,13 +261,16 @@ document.addEventListener('DOMContentLoaded', function() {
         display_box_text.textContent = phone_updatedtext;
         display_box_text.classList.add('smooth-transition');
         icon_box_list[3].style.backgroundColor = '#e8e0d3';
+        icon_box_list[3].classList.add('bounce');
     });
     phone_.addEventListener("mouseout", function(){
         display_box_text.textContent = original_text;
         display_box_text.classList.remove('smooth-transition');
         icon_box_list[3].style.backgroundColor = '#f8f0e3';
+        icon_box_list[3].classList.remove('bounce');
     });
 
-
+  }
   });
   
+
