a6f63669ae67230dd873e3fafeace7e15cdb14e2
TanPhuocHuynh <78418185+tanhuynh230399@users.noreply.github.com>
2024-01-29 13:13:06 +1100

    Add files via upload

---
+++ Week4/src/App.js
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import AddTask from "./AddTask.js";
+import TaskList from "./TasksList.js";
+import { useReducer } from "react";
+nextId = 0;
+initialTasks = [];
+export default function TaskApp() {
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+
+  function handleAddTask(text) {
+    dispatch({
+      type: "added",
+      id: nextId++,
+      text: text,
+    });
+  }
+
+  function handleChangeTask(task) {
+    dispatch({
+      type: "changed",
+      task: task,
+    });
+  }
+
+  function handleDeleteTask(taskId) {
+    dispatch({
+      type: "deleted",
+      id: taskId,
+    });
+  }
+
+  return (
+    <>
+      <h2>Todo List</h2>
+      <AddTask onAddTask={handleAddTask} />
+      <p>Your Tasks:</p>
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
+    </>
+  );
+}
+
+function tasksReducer(tasks, action) {
+  switch (action.type) {
+    case "added": {
+      return [
+        ...tasks,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    }
+    case "changed": {
+      return tasks.map((t) => {
+        if (t.id === action.task.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
+    }
+    case "deleted": {
+      return tasks.filter((t) => t.id !== action.id);
+    }
+    default: {
+      throw Error("Unknown action: " + action.type);
+    }
+  }
+}
